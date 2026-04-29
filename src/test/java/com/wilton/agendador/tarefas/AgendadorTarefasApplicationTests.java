b9ab7e45d38472276c19a8127c0c6e4040ed979c
Wilton <wiltonjunioralvess@gmail.com>
2026-04-29 14:02:10 -0400

    commit inicial

---
+++ src/test/java/com/wilton/agendador/tarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilton.agendador.tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
