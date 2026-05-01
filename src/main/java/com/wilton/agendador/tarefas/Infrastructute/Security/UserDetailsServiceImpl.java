8558034b7ceed9913baac50c540558feb3b580ad
Wilton <wiltonjunioralvess@gmail.com>
2026-05-01 13:41:08 -0400

    adicionei configuraçoes de segurança

---
+++ src/main/java/com/wilton/agendador/tarefas/Infrastructute/Security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.wilton.agendador.tarefas.Infrastructute.Security;
+
+
+
+import com.wilton.agendador.tarefas.Business.dto.UsuarioDTO;
+import com.wilton.agendador.tarefas.Infrastructute.client.UsuarioCliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl  {
+
+    @Autowired
+    private UsuarioCliente cliente;
+
+
+    public UserDetails carregaDadosDeUsuario(String email, String token) {
+
+        UsuarioDTO usuarioDTO = cliente.buscaUsuarioEmail(email, token);
+        return User
+                .withUsername(usuarioDTO.getEmail()) // Define o nome de usuário como o e-mail
+                .password(usuarioDTO.getSenha()) // Define a senha do usuário
+                .build();
+    }
+}
